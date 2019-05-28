b1489c594141bf83123c6bd5f7e5320566a5ae7b
Ryan Rowland <w.rowland.ryan@gmail.com>
2019-05-28 15:47:36 -0500

    Added layout placeholder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Layout from './components/Layout/Layout';
 // import './App.css';
 
 function App() {
   return (
     <div>
-      TODO
+      <Layout>
+        <p>TODO</p>
+      </Layout>
     </div>
   );
 }
